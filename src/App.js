c0f788609ed42a431826db758fae67a57c7c94bf
ariellongoria <ariello97@hotmail.com>
2021-10-07 17:01:14 -0500

    im done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [currentSection, setCurrentSection] = useState("aboutme");
+
+    return (
+        <div>
+            <header>
+                <Nav currentSection={currentSection} setCurrentSection={setCurrentSection} />
+                {/*if currentSection changes, the rendered component changes*/}
+            </header>
+            <main>
+                {currentSection === "aboutme" ? (
+                    <About />
+                ) : currentSection === "projects" ? (
+                    <Projects />
+                ) : currentSection === "resume" ? (
+                    <Resume />
+                ) : (
+                    <Contact />
+                )}
+            </main>
+            <footer>
+                <span>Made by Ariel Longoria</span>
+                <a href="github.com/ariellongoria">GitHub</a>
+                <a href="linkedin.com/in/ariellongoria">LinkedIn</a>
+                <a href="/">Lmao</a>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
